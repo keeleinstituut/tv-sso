689c42456e895e139c14829dfe4e33c1fc82455c
dmytro <dmytro@fluidum.ee>
2025-12-11 14:28:53 +0200

    Fix tests for keycloak v26

--- keycloak-tv-extensions/src/main/java/ee/eki/tolkevarav/sso/keycloakserviceprovider/util/serviceaccountfetcher/ServiceAccountFetcher.java
+++ keycloak-tv-extensions/src/main/java/ee/eki/tolkevarav/sso/keycloakserviceprovider/util/serviceaccountfetcher/ServiceAccountFetcher.java
@@ -1,16 +1,21 @@
 package ee.eki.tolkevarav.sso.keycloakserviceprovider.util.serviceaccountfetcher;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.jboss.logging.Logger;
-import org.keycloak.jose.jws.JWSBuilder;
-import org.keycloak.models.ClientModel;
+import org.keycloak.TokenVerifier;
+import org.keycloak.common.VerificationException;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.UserModel;
 import org.keycloak.representations.AccessToken;
-import org.keycloak.services.Urls;
+import org.keycloak.util.JsonSerialization;
 
+import java.io.IOException;
 import java.net.URI;
-import java.util.Set;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class ServiceAccountFetcher {
 
@@ -27,52 +32,57 @@ public class ServiceAccountFetcher {
     public String getServiceAccountAccessToken() throws RuntimeException {
         var realm = this.keycloakSession.getContext().getRealm();
         var client = this.keycloakSession.clients().getClientByClientId(realm, this.clientId);
+        var httpClient = HttpClient.newHttpClient();
         
         if (client == null) {
             throw new RuntimeException("Client not found: " + clientId);
         }
 
         try {
-            // Get the service account user for this client (Keycloak 26 API)
-            UserModel serviceAccountUser = this.keycloakSession.users().getServiceAccount(client);
-            if (serviceAccountUser == null) {
-                throw new RuntimeException("Service account user not found for client: " + clientId);
-            }
-
-            // Create access token using Keycloak's internal APIs
-            AccessToken token = new AccessToken();
-            token.audience(client.getClientId());
-            token.subject(serviceAccountUser.getId());
-            token.issuedFor(client.getClientId());
-            token.type("Bearer");
+            String clientSecret = client.getSecret();
             
-            URI baseUri = this.keycloakSession.getContext().getUri().getBaseUri();
-            token.issuer(Urls.realmIssuer(baseUri, realm.getName()));
-            token.issuedNow();
-            token.exp((long) (token.getIat() + 600)); // Token valid for 10 minutes
-
-            // Add client roles to the token
-            Set<String> clientRoles = this.keycloakSession.roles().getClientRolesStream(client)
-                .map(role -> role.getName())
-                .collect(java.util.stream.Collectors.toSet());
-            AccessToken.Access realmAccess = new AccessToken.Access();
-            for (String role : clientRoles) {
-                realmAccess.addRole(role);
+            if (clientSecret == null || clientSecret.isEmpty()) {
+                logger.errorf("Client secret is null or empty for client: %s", this.clientId);
+                throw new RuntimeException("Client secret is not available for client: " + this.clientId);
             }
-            token.setRealmAccess(realmAccess);
 
-            // Sign the token using Keycloak's internal signing mechanism
-            var activeRsaKey = this.keycloakSession.keys().getActiveRsaKey(realm);
-            String signedToken = new JWSBuilder()
-                .kid(activeRsaKey.getKid())
-                .type("JWT")
-                .jsonContent(token)
-                .rsa256(activeRsaKey.getPrivateKey());
+            // When running inside Keycloak container, use internal address (localhost:8080)
+            // instead of external mapped port from getBaseUri()
+            String baseUri = this.keycloakSession.getContext().getUri().getBaseUri().toString();
+            // Replace host:port with localhost:8080 for internal container communication
+            URI baseUriObj = URI.create(baseUri);
+            String internalBaseUri = String.format("%s://%s:%d/", 
+                baseUriObj.getScheme(), 
+                "localhost", 
+                8080);
+            var tokenEndpoint = new URI("%srealms/%s/protocol/openid-connect/token".formatted(internalBaseUri, realm.getName()));
 
-            return signedToken;
+            var auth = client.getClientId() + ":" + clientSecret;
+            var encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.UTF_8));
+            var body = "grant_type=client_credentials";
+            var request = HttpRequest
+                    .newBuilder(tokenEndpoint)
+                    .setHeader("Content-Type", "application/x-www-form-urlencoded")
+                    .setHeader("Authorization", "Basic " + encodedAuth)
+                    .POST(HttpRequest.BodyPublishers.ofString(body))
+                    .build();
 
-        } catch (Exception e) {
-            logger.error("Encountered error when generating access token for service account", e);
+            var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            
+            if (response.statusCode() != 200) {
+                String errorMsg = String.format("Failed to obtain service account token for client '%s'. Status: %d, Body: %s", 
+                    this.clientId, response.statusCode(), response.body());
+                logger.errorf(errorMsg);
+                throw new RuntimeException("Failed to obtain service account token. HTTP status: " + response.statusCode());
+            }
+            
+            var jsonResponse = JsonSerialization.readValue(response.body(), JsonNode.class);
+            String accessTokenJwt = jsonResponse.get("access_token").asText();
+            TokenVerifier.create(accessTokenJwt, AccessToken.class).getToken();
+            return accessTokenJwt;
+
+        } catch (IOException | InterruptedException | VerificationException | URISyntaxException e) {
+            logger.error("Encountered error when fetching access token for service account", e);
             throw new RuntimeException(e);
         }
     }
